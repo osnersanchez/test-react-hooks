00114055b083ddd138fdc76a141f684aa02e14e7
Osner Sanchez <osner.sanchez@gmail.com>
2020-10-19 22:52:17 -0500

    add build docs

---
+++ docs/precache-manifest.6508af78b1698defe35e4045a162d075.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef43fef2572a331b6fb226c62cfeeb3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d799916664f7adf101d7",
+    "url": "/static/css/main.e411ab4f.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d799916664f7adf101d7",
+    "url": "/static/js/main.45f67b45.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
